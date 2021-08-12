32853bcdc5f8017b3394ea1943297cf2d34ee94f
kcwong232485 <kcwong232485@hotmail.com>
2021-08-12 08:03:13 +0800

    fix duplicate id problem for ratingstar

--- src/components/Ratingstar.vue
+++ src/components/Ratingstar.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container">      
       <div class="star-widget">
-        <input type="radio" name="rate" id="rate-5" @click="starclick(5)">
-        <label for="rate-5" class="fas fa-star"></label>
-        <input type="radio" name="rate" id="rate-4" @click="starclick(4)">
-        <label for="rate-4" class="fas fa-star"></label>
-        <input type="radio" name="rate" id="rate-3" @click="starclick(3)">
-        <label for="rate-3" class="fas fa-star"></label>
-        <input type="radio" name="rate" id="rate-2" @click="starclick(2)">
-        <label for="rate-2" class="fas fa-star"></label>
-        <input type="radio" name="rate" id="rate-1" @click="starclick(1)">
-        <label for="rate-1" class="fas fa-star"></label>        
+        <input type="radio" v-bind:name="myname" v-bind:id="myrate5" @click="starclick(5)">
+        <label v-bind:for="myrate5" class="fas fa-star" :style="ratingStyle(5)"></label>
+        <input type="radio" v-bind:name="myname" v-bind:id="myrate4" @click="starclick(4)">
+        <label v-bind:for="myrate4" class="fas fa-star" :style="ratingStyle(4)"></label>
+        <input type="radio" v-bind:name="myname" v-bind:id="myrate3" @click="starclick(3)">
+        <label v-bind:for="myrate3" class="fas fa-star" :style="ratingStyle(3)"></label>
+        <input type="radio" v-bind:name="myname" v-bind:id="myrate2" @click="starclick(2)">
+        <label v-bind:for="myrate2" class="fas fa-star" :style="ratingStyle(2)"></label>
+        <input type="radio" v-bind:name="myname" v-bind:id="myrate1" @click="starclick(1)">
+        <label v-bind:for="myrate1" class="fas fa-star" :style="ratingStyle(1)"></label>        
       </div>
-      <div class="rating"> {{ rating }} </div>      
+      <div class="rating"> {{ myrating }} </div>      
     </div>
 </template>
 <script>
@@ -21,20 +21,33 @@
   components: {
      
   },
+  props: [ 'myrating', 'mymovieid', 'myrate5', 'myrate4', 'myrate3', 'myrate2', 'myrate1', 'myname' ],
   data() {
     return {
-      rating: 0,
+      //rating: 0,
     }
+  },
+  computed: {
+    
   },
   methods: {
     starclick(i) {
-        if (this.rating == 1) {
-            let elem = document.getElementById("rate-1")
+      /*
+        if (this.rating == 1 && i == 1) {
+            let elem = document.getElementById(this.myrate1)
             elem.checked = false
-            this.rating = 0
+            this.myrating = 0
             return
         }
         this.rating = i
+      */
+     //console.log('mymovieid: ' + this.mymovieid)
+     this.$emit('starclicked', i, this.mymovieid)
+    }, 
+    ratingStyle(j) {
+      if (this.myrating > j-1) {
+        return {color: '#fd4'}
+      }
     }
   }
 }
@@ -78,6 +91,7 @@ input:not(:checked) ~ label:hover,
 input:not(:checked) ~ label:hover ~ label{
   color: #fd4;
 }
+/*
 input:checked ~ label{
   color: #fd4;
 }
@@ -85,5 +99,5 @@ input#rate-5:checked ~ label{
   color: #fe7;
   text-shadow: 0 0 20px #952;
 }
-
+*/
 </style>
